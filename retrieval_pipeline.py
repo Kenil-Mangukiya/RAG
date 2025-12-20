53bee92b334e99484f99628267805ec96fd584b2
Kenil-Mangukiya <kenilmangukiya033@gmail.com>
2025-12-20 22:52:16 +0530

    Integrated llm to output structured response

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -1,6 +1,8 @@
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
@@ -24,3 +26,25 @@ print(f"User query is : {query}")
 print("---Context---")
 for i, doc in enumerate(relevant_docs, 1):
     print(f"Document {i} : \n{doc.page_content}")
+
+prompt = f"""Based on the following documents, please answer this question: {query}
+enAI(model="gpt-4o")
+
+Documents:
+{chr(10).join([f"- {doc.page_content}" for doc in  relevant_docs])}
+
+Please provide a clear, helpful answer using only the information from these documents.if you can't find the answer in the in the documents, say "I don't have enough information to answer that question based on the provided documents
+"""
+
+model = ChatOp
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content=prompt)
+]
+
+result = model.invoke(messages)
+
+print("Generated response : ")
+print(f"result is : {result}")
+print("Content only : ")
+print(result.content)
